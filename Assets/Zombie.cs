a3a5560a52eb9946d606cd7d7df31e066b6916de
Nikita Gupta <nikita2302@gmail.com>
2021-04-14 23:20:43 -0400

    Zombie starts from random position

--- Assets/Zombie.cs
+++ Assets/Zombie.cs
@@ -10,6 +10,9 @@ public class Zombie : MonoBehaviour
     void Start()
     {
        // animator = GetComponent<Animator>();
+       
+       //var randomVector = new Vector3(Random.Range(0.5f, 1f), 0.5f, Random.Range(0.5f, 1f));
+       //transform.position = randomVector;
     }
 
     // Update is called once per frame
